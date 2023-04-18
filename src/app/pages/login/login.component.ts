409330862c6fcefcbb16e00896c760a21854aa80
Lucas Newlands <lucasnewlands@MacBook-Pro-de-Lucas.local>
2023-04-18 18:16:07 -0300

    feat: redirect to spotify authentication

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private SpotifyService: SpotifyService) { }
+
+  openLoginPage(){
+    console.log(this.SpotifyService.getUrlLogin())
+  }
 }
